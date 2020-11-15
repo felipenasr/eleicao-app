17bca889211ad9cabca1aab9cf322d801fbdf08a
Felipe <fricci>
2020-11-15 20:15:35 -0300

    a

--- src/app/eleicao/estados/estados.component.spec.ts
+++ src/app/eleicao/estados/estados.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { EstadosComponent } from './estados.component';
 
@@ -6,7 +6,7 @@ describe('EstadosComponent', () => {
   let component: EstadosComponent;
   let fixture: ComponentFixture<EstadosComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ EstadosComponent ]
     })
